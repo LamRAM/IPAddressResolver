d243c4966fc6fb58d026ef7242e70e4d1436ad72
lamiaa <l.alqahtani@safcsp.org.sa>
2023-11-14 12:30:12 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static String getIpByHostName(String hostName){
+        String ipAddress = null;
+        /* Your code here */
+        return ipAddress;
+    }
+    public static void main(String[] args) {
+        /* Main.getIpByHostName(hostName) */
+    }
+}
